18f49081a69b424eb45eb3b1f21fe4bdaa3afe5c
arnaud <arnaudcoubran@gmail.com>
2023-11-20 22:36:07 +0100

    Create wik_dps_tp01.d

---
+++ target/release/wik_dps_tp01.d
@@ -0,0 +1,1 @@
+/Users/arnaudcoubran/Documents/Cours\ B3/Devops/Conteneurisation/wik-dps-tp02/target/release/wik_dps_tp01: /Users/arnaudcoubran/Documents/Cours\ B3/Devops/Conteneurisation/wik-dps-tp02/src/main.rs
